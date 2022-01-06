704f97b8f5cc5dd3e4910320884388af366ee26e
jj <daylay6@naver.com>
2022-01-06 19:11:09 +0900

    20220106_SW

---
+++ lje/20220106_SW.py
@@ -0,0 +1,28 @@
+#SW Expert Academy
+
+#문제1
+#지그재그 숫자 D2
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV5PxmBqAe8DFAUq&categoryId=AV5PxmBqAe8DFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=2&pageSize=10&pageIndex=1&&&&&&&&&&
+
+t = int(input())
+for i in range(1,t+1):
+    n = int(input())
+    answer = 0
+    for j in range(1,n+1):
+        if j%2 == 0:
+            answer -= j
+        else:
+            answer += j
+    print(f'#{i} {answer}')
+
+
+#문제2
+# 간단한 369게임 D2
+#https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV5PTeo6AHUDFAUq&categoryId=AV5PTeo6AHUDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=2&pageSize=10&pageIndex=1&&&&&&&&&&
+
+n = int(input())
+answer = ''
+for i in range(1,n+1):
+    if i in [3,6,9]:
+        answer += '-'
+print(answer)
