285176c046029c29794ee48a508323aba535f246
James <deliver0713@gmail.com>
2022-01-06 15:07:39 +0900

    20220106

--- ljk/20220106.py
+++ ljk/20220106.py
@@ -9,8 +9,29 @@ for i in range(int(input())):
     print(f'#{i+1} {result}')
 
 #369게임
-for i in range(int(input())):
-    if '3' or 
+import re
+for i in range(1,int(input())+1):
+    j=str(i)
+    if '3' in j or '6' in j or '9' in j:
+        j=re.sub('[369]','-',j)
+        j=re.sub('[^-]','',j)
+        print(j, end=' ')
+    else:
+        print(i, end=' ')
 
-print(1, end='')
-print(2, end=' ')
+########################################정규표현식이 적용이 안됨
+
+
+for i in range(1,int(input())+1):
+    j=str(i)
+    if '3' in j or '6' in j or '9' in j:
+        answer=''
+        for k in j:
+            if k=='3' or k=='6' or k=='9':
+                answer+='-'
+            else:
+                answer+=''
+        print(answer, end=' ')
+    else:
+        print(i, end=' ')
+
