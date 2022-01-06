ac8dd9ccc2f31605774f48ca25953cf3e83bc2da
James <deliver0713@gmail.com>
2022-01-06 14:19:15 +0900

    12

---
+++ ljk/20220106.py
@@ -0,0 +1,16 @@
+#지그재그게임
+for i in range(int(input())):
+    result=0
+    for j in range(1,int(input())+1):
+        if j%2==0:
+            result-=j      
+        else:
+            result+=j  
+    print(f'#{i+1} {result}')
+
+#369게임
+for i in range(int(input())):
+    if '3' or 
+
+print(1, end='')
+print(2, end=' ')
