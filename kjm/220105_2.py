cae48695b8dbbd2547a851ddb59bcfe654b45460
jamin-rachel-kim <91674435+jamin-rachel-kim@users.noreply.github.com>
2022-01-07 13:01:48 +0900

    Update and rename diagonal.py to 220105_2.py

---
+++ kjm/220105_2.py
@@ -0,0 +1,6 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&problemLevel=2&contestProbId=AV5QFuZ6As0DFAUq&categoryId=AV5QFuZ6As0DFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=2&pageSize=10&pageIndex=2
+# SW Expert Academy 2027.
+
+for i in range(5):
+    a = "+"*i + "#" + "+"*(4-i)
+    print(a)
--- kjm/diagonal.py
+++
@@ -1,3 +0,0 @@
-for i in range(5):
-    a = "+"*i + "#" + "+"*(4-i)
-    print(a)
