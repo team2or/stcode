5076123bdd03d2b0106a659011d9303bfec6fc07
Jaekwan Lee <88877270+JamesLeeeeeee@users.noreply.github.com>
2022-02-09 22:36:59 +0900

    Update 20220209.py

--- ljk/20220209.py
+++ ljk/20220209.py
@@ -6,11 +6,9 @@ for _ in range(m):
     package, one=map(int, input().split())
     packages.append(package)
     ones.append(one)
-if min(packages)> min(ones)*(n%6):
-    print((n//6)*min(packages)+(n%6)*min(ones))
-elif ((n//6)+1)*min(packages) > n*min(ones):
-    print(n*min(ones))
-else:
-    print(((n//6)+1)*min(packages))
+pack_min=min(packages)
+one_min=min(ones)
+print(min([pack_min*(n//6+1), pack_min*(n//6)+one_min*(n%6), one_min*n, ]))
+
 
 
