813cccf614e24c6adca6ac35cda5d5584ed4c946
jj <daylay6@naver.com>
2022-02-15 23:44:59 +0900

    20220215

--- lje/20220215.py
+++ lje/20220215.py
@@ -12,17 +12,37 @@ SELECT ANIMAL_ID FROM ANIMAL_INS WHERE NAME IS NOT NULL ORDER BY ANIMAL_ID;
 #백준
 #도서관
 #https://www.acmicpc.net/problem/1461
-n, m = map(int, input().split())
+n,m = map(int,input().split())
 p = list(map(int, input().split()))
-minus = sorted([])
-plus = sorted([], reverse=True)
-max_din = abs(max(p))
+minus = []
+plus = []
+max_din = 0
+
 for i in p:
     if i > 0:
         plus.append(i)
     else:
         minus.append(i)
 
-work = 0
-for j in range(0, 5, 3):
-    print(j)
+    if abs(i)>abs(max_din):
+        max_din=i
+
+minus.sort()
+plus.sort(reverse=True)
+
+distances = []
+
+for i in range(0, len(plus), m):
+    if plus[i] != max_din:
+        distances.append(plus[i])
+
+for i in range(0, len(minus), m):
+    if minus[i] != max_din:
+        distances.append(minus[i])
+
+answer = abs(max_din)
+for i in distances:
+    answer += abs(i * 2)
+
+
+print(answer)
