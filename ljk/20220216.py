0d6af0ed7d8147c5b270a842640767f284cc146f
Jaekwan Lee <88877270+JamesLeeeeeee@users.noreply.github.com>
2022-02-17 02:12:45 +0900

    Add files via upload

--- ljk/20220216.py
+++ ljk/20220216.py
@@ -1,20 +1,37 @@
-n=int(input())
+#1만들기
 
-list=[]
-cnt=0
-while n!=1:
-    list.append(n)
-    if n%3==0:
-        n=n//3
-    elif n%2==0:
-        if (n-1)%3==0:
-            n=n-1        
-        else:
-            n=n//2
-    else:
-        n-=1
-    cnt+=1
-list.append(1)
-print(cnt)
-for i in list:
-    print(i,end=' ')
+n=int(input())
+dp=[0]*(n+1)
+dp[0]=-1
+for i in range(1,n+1):
+    if dp[i]==0:
+        dp[i]=dp[i-1]+1
+        if i%3==0:
+            if dp[i]>dp[i//3]+1:
+                dp[i]=dp[i//3]+1
+        if i%2==0:
+            if dp[i]>dp[i//2]+1:
+                dp[i]= dp[i//2]+1
+print(dp[n])
+
+
+
+#1만들기 2
+
+n=int(input())
+dp=[[0,0]]*(n+1)
+dp[0]=[-1,-1]
+for i in range(1,n+1):
+    dp[i]=[dp[i-1][0]+1, i-1]
+    if i%2==0:
+        if dp[i][0]>dp[i//2][0]+1:
+            dp[i]=[dp[i//2][0]+1, i//2]
+    if i%3==0: 
+        if dp[i][0] > dp[i//3][0]+1:
+            dp[i]=[dp[i//3][0]+1, i//3]
+print(dp[n][0])
+min_, go_one= dp[n]
+print(n,end=' ')
+while go_one!=0:
+    print(go_one, end=' ')
+    min_, go_one=dp[go_one]
