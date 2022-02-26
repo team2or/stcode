68a36e4b6a42a3afc90fa5c86baddbd782bd53f9
Jaekwan Lee <88877270+JamesLeeeeeee@users.noreply.github.com>
2022-02-26 15:09:47 +0900

    Add files via upload

--- ljk/20220225.py
+++ ljk/20220225.py
@@ -3,19 +3,22 @@
 n= int(input())
 seats=input().upper()
 cups=''
+cnt=0
 for seat in seats:
     if seat== 'S':
         if cups:
            if cups[-1]=='*':
                 cups+='*'
+                cnt+=1
         else:
             cups+='*'*2
     elif seat=='L':
         if cups:
             if cups[-1]=='*':
                 cups+=' '
+                cnt+=1
             elif cups[-1]==' ':
                 cups+='/'
             elif cups[-1]=='/':
                 cups+='*'
-print(cups.count('*'))
+print(cnt)
