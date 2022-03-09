c329e5e0f0ca0498d08b88bb259d81182cea683c
Jaekwan Lee <88877270+JamesLeeeeeee@users.noreply.github.com>
2022-03-09 15:56:31 +0900

    Add files via upload

--- ljk/20220307.py
+++ ljk/20220307.py
@@ -1,1 +1,26 @@
 #타겟 넘버
+def solution(numbers, target):
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    dfs(0,0)
+    return answer
+
+
+
+if __name__=="__main__":
+    numbers=[1, 1, 1, 1, 1]
+    target=3
+    ans=solution(numbers, target)
+    print(ans)
+
+
+
