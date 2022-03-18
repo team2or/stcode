5f743c9dacbe6525ec0972addf2f930112acdbf5
Jaekwan Lee <88877270+JamesLeeeeeee@users.noreply.github.com>
2022-03-19 01:27:58 +0900

    Add files via upload

--- ljk/20220318.py
+++ ljk/20220318.py
@@ -1,14 +1,51 @@
 #거꾸로 구구단 
 
+# import sys
+# def solution():
+#     answer='0'
+#     n,k = map(int, sys.stdin.readline().split())
+#     maxlen= len(str(n*k))
+
+#     for i in range(1,k+1):
+#         if len(str(i*n))== maxlen:
+#             no=str(i*n)
+#             if int(no[-1]) >= int(answer[0]):
+#                 if len(no)%2!=0:
+#                     j=len(no)//2
+#                     if j==0:
+#                         if int(answer)< int(no):
+#                             answer=no
+#                     else:
+#                         for r in range(1,j+1):
+#                             no=list(no)
+#                             no[j+r],no[j-r]=no[j-r],no[j+r]
+#                             no=''.join(no)
+#                             if int(answer)<int(no):
+#                                 answer=no
+#                 else:
+#                     for j in range(maxlen//2):
+#                         no=list(no)
+#                         no[j],no[maxlen-1-j]= no[maxlen-1-j], no[j]
+#                         no=''.join(no)
+#                         if int(answer)<int(no):
+#                             answer=no
+#     return int(answer)
+
+
+# if __name__=="__main__":
+#     hats=solution()
+#     print(hats)
+
 import sys
-def solution():
-    answer=0
-    n,k = map(int, sys.stdin.readline().split())
-    
-    
+class solution:
+    def __init__(self):
+        self.n, self.k = map(int,sys.stdin.readline().split())
+        self.answer()
+    def answer(self):
+        maxlen=len(str(self.n*self.k))
+        answer = [int(str(i*self.n)[::-1]) for i in range(1,self.k+1) if len(str(i*self.n)) >= maxlen]
+        answer.sort()
+        print(answer[-1])
 
 if __name__=="__main__":
-    hats=solution()
-    for hat in hats:
-        print(hat)
-
+    solution()
