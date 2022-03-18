799e2c04f43f3c958abbb7d055a109028b2b3be2
Jaekwan Lee <88877270+JamesLeeeeeee@users.noreply.github.com>
2022-03-18 23:34:12 +0900

    Add files via upload

---
+++ ljk/20220318.py
@@ -0,0 +1,14 @@
+#거꾸로 구구단 
+
+import sys
+def solution():
+    answer=0
+    n,k = map(int, sys.stdin.readline().split())
+    
+    
+
+if __name__=="__main__":
+    hats=solution()
+    for hat in hats:
+        print(hat)
+
