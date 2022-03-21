b8893b1fa873022c19274b66534ef3be0d831283
Jaekwan Lee <88877270+JamesLeeeeeee@users.noreply.github.com>
2022-03-22 01:58:52 +0900

    Add files via upload

--- ljk/20220321.py
+++ ljk/20220321.py
@@ -1,1 +1,34 @@
 #촌수계산
+import sys
+from collections import deque
+n= int(sys.stdin.readline())
+a,b= map(int, sys.stdin.readline().split())
+m= int(sys.stdin.readline())
+arr=[[] for _ in range(n+1)]
+result= [0]*(n+1)
+for _ in range(m):
+    x,y= map(int, sys.stdin.readline().split())
+    arr[x].append(y)
+    arr[y].append(x)
+
+def bfs(s):
+    q= deque()
+    visited = [0 for _ in range(n+1)]
+
+    q.append(s)
+    visited[s] = 1
+
+    while q:
+        x = q.popleft()
+
+        for i in arr[x]:
+            if visited[i]==0:
+                visited[i]=1
+                result[i] = result[x]+1
+                q.append(i)
+
+bfs(a)
+if result[b] !=0 :
+    print(result[b])
+else:
+    print(-1)
