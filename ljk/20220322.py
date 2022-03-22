d23f8f09ccc5570e1c4f77d39bdeb1d4701d8502
Jaekwan Lee <88877270+JamesLeeeeeee@users.noreply.github.com>
2022-03-23 01:47:40 +0900

    Add files via upload

--- ljk/20220322.py
+++ ljk/20220322.py
@@ -1,27 +1,43 @@
 #연결 요소의 개수
 from sys import stdin
+from collections import deque
+import sys
+sys.setrecursionlimit(10000)
 class ans:
     def __init__(self):
-        self.solution()
-
+        answer=self.solution()
+        print(answer)
     def solution(self):
         n, m= map(int, stdin.readline().split())
-        arr=[[] for _ in range(n+1)]
+        self.arr=[[] for _ in range(n+1)]
         for _ in range(m):
             x,y = map(int, stdin.readline().split())
-            arr[x].append(y)
-            arr[y].append(x)
+            self.arr[x].append(y)
+            self.arr[y].append(x)
         self.result=[0]*(n+1)
         answer=0
-        for i,j in enumerate(arr):
+        for i in range(1, n+1):
             if self.result[i]==0:
-                self.result[i]=1
-            for k in j:
-                self.bang(k)
+                answer+=1
+                self.bang(i)
+        return answer
+    # def bang(self, i):
+    #     queue= deque(self.arr[i])
+    #     self.result[i]=1
+    #     while queue:
+    #         v= queue.popleft()
 
-    def bang(self, num):
-        while self.result[num]:
-            self.result[num].pop(0)
+    #         for h in self.arr[v]:
+    #             if self.result[h]!=1:
+    #                 queue.append(h)
+    #                 self.result[h]=1
+
+    def bang(self, i):
+        self.result[i]=1
+        for h in self.arr[i]:
+            if self.result[h]==0:
+                self.bang(h)
+
 
 
 
