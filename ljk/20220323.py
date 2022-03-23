b5c2fd5f949e8f4f8ba9416479e04342d27c546f
Jaekwan Lee <88877270+JamesLeeeeeee@users.noreply.github.com>
2022-03-24 04:05:53 +0900

    Add files via upload

--- ljk/20220323.py
+++ ljk/20220323.py
@@ -6,11 +6,30 @@ class Tomato:
         m,n = map(int, stdin.readline().split())
         tomatos=[list(map(int, stdin.readline().split())) for _ in range(n)]
         queue= deque([])
-        dx, xy= [-1,1,0,0],[0,0,-1,1]
+        dx, dy= [-1,1,0,0],[0,0,-1,1]
         answer=0
 
         for i in range(n):
             for j in range(m):
+                if tomatos[i][j]==1:
+                    queue.append([i,j])
+        self.bfs(queue,dx,dy, tomatos,n,m)
+        for i in tomatos:
+            for j in i:
+                if j==0:
+                    print(-1)
+                    exit(0)
+            answer=max(answer, max(i))
+        print(answer-1)
+
+    def bfs(self, queue,dx, dy, tomatos,n,m):
+        while queue:
+            x,y = queue.popleft()
+            for i in range(4):
+                nx, ny = dx[i]+x, dy[i]+y
+                if 0<= nx <n and 0<= ny< m and tomatos[nx][ny]==0:
+                    tomatos[nx][ny]=tomatos[x][y]+1
+                    queue.append([nx,ny])
 
 
 
