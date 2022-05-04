66a5f0730dc98c3a45018a877ebe03823d435f1a
Jaekwan Lee <88877270+JamesLeeeeeee@users.noreply.github.com>
2022-05-05 00:42:17 +0900

    Add files via upload

--- ljk/20220504.py
+++ ljk/20220504.py
@@ -1,15 +1,24 @@
 from collections import deque
-def solution(people, limit):
-    answer = 0
-    deq = deque(sorted(people))
-    while deq:
-        if len(deq) == 1:
-            answer += 1
-            break
-        if deq[0] + deq[-1] <= limit:
-            deq.pop()
-            deq.popleft()
-        else:
-            deq.pop()
-        answer += 1
+def solution(n):
+    top = [[0]*n for _ in range(n)]
+    x, y = -1, 0
+    num = 1
+    for i in range(n):
+        for _ in range(i, n):
+            if i % 3 == 0:  # 아래
+                x += 1
+            elif i % 3 == 1:  # 오른쪽
+                y += 1
+            elif i % 3 == 2:  # 위쪽 대각선
+                x -= 1
+                y -= 1
+            top[x][y] = num
+            num += 1
+    answer = []
+    for i in top:
+        for j in i:
+            if j:
+                answer.append(j)
+    print(answer)
     return answer
+solution(4)
