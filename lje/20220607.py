da6453b2e4dabf02686b7cec3ece448fa35ca3fc
jj <daylay6@naver.com>
2022-06-07 20:32:12 +0900

    20220607

---
+++ lje/20220607.py
@@ -0,0 +1,30 @@
+#해커랭크
+#복습
+#Revising the Select Query I
+#https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+#SELECT * FROM CITY WHERE CountryCode = "USA" and population > 100000;
+
+#프로그래머스
+#피보나치수열
+#멀리뛰기
+#https://programmers.co.kr/learn/courses/30/lessons/12914
+#답안: https://hwayomingdlog.tistory.com/101?category=939394
+def solution(n):
+    dp = [0]*(n+1)
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2, n+1):
+        dp[i] = (dp[i-2] + dp[i-1])%1234567
+    return dp[n]
+    
+#다른답안
+#https://velog.io/@wjdtmdgml/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4%EB%A9%80%EB%A6%AC%EB%9B%B0%EA%B8%B012914%EB%B2%88Python%ED%8C%8C%EC%9D%B4%EC%8D%ACDP
+def solution(n):
+    if n<3:
+        return n
+    d=[0]*(n+1)
+    d[1]=1
+    d[2]=2
+    for i in range(3,n+1):
+        d[i]=d[i-1]+d[i-2]
+    return d[n]%1234567
